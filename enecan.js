15f381d481bfff9b51429258bc3205dc7de220f0
RJ <nasulrich@gmail.com>
2018-02-23 12:11:40 +0900

    add count playtime

--- enecan.js
+++ enecan.js
@@ -1,74 +1,107 @@
-
-        $.ajaxSetup({
-            beforeSend: function(xhr) {
-                xhr.overrideMimeType("text/html;charset=Shift_JIS");
-            }
-        });
-
-
-        $("#drop-area").on("drop", function(e) {
-            var oneDay = "";
-            e.preventDefault();
-            var files = e.originalEvent.dataTransfer.files
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                $("#file").html("TWチャットログファイル名：" + "<strong>" + file.name + "</strong>");
-                oneDay = new Date((file.name).substr(10, 4), (file.name).substr(15, 2) - 1, (file.name).substr(18, 2));
+$.ajaxSetup({
+    beforeSend: function (xhr) {
+        xhr.overrideMimeType("text/html;charset=Shift_JIS");
+    }
+});
 
 
-            }
-            var reader = new FileReader();
-            // onloadハンドラ
-            reader.onload = function(e) {
-                // 取得した結果を表示
-                var res = e.target.result;
-                var str = res.split("</br>")
-                var strArray = new Array();
-                var mobCount = 0;
-                var total = 0;
-                var exp = 0;
-                var flg = true;
-                var dateA = "";
-                var dateB = "";
-                var dateC = "";
-                var all = 0;
-                for (var i = 0; i < str.length; i++) {
-                    strArray[i] = str[i];
-                    if (strArray[i].indexOf("経験値が") > 0) {
-                        if (strArray[i].indexOf("ルーン経験値が") > 0) {
-                            continue;
-                        }
-                        mobCount += 1;
-
-                        if (flg == true) {
-                            dateA = new Date(oneDay.getFullYear(), oneDay.getMonth(), oneDay.getDate(), strArray[i].substr(strArray[i].indexOf("[") + 1, 2), strArray[i].substr(strArray[i].indexOf("[") + 5, 2), strArray[i].substr(strArray[i].indexOf("[") + 9, 2));
-                            dateB = new Date(dateA);
-                            dateB.setHours(dateA.getHours() + 1);
-                            flg = false;
-                            console.log(dateA);
-                            console.log(dateB);
-                        }
-                        dateC = new Date(oneDay.getFullYear(), oneDay.getMonth(), oneDay.getDate(), strArray[i].substr(strArray[i].indexOf("[") + 1, 2), strArray[i].substr(strArray[i].indexOf("[") + 5, 2), strArray[i].substr(strArray[i].indexOf("[") + 9, 2));
+$("#drop-area").on("drop", function (e) {
+    var oneDay = "";
+    e.preventDefault();
+    var files = e.originalEvent.dataTransfer.files
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        $("#file").html("TWチャットログファイル名：" + "<strong>" + file.name + "</strong>");
+        oneDay = new Date((file.name).substr(10, 4), (file.name).substr(15, 2) - 1, (file.name).substr(18, 2));
 
 
-                        //console.log(parseInt(strArray[i].slice(strArray[i].indexOf("経験値が ")+4,strArray[i].lastIndexOf(" 上がりました。</font>"))));
-                        //経験値
-                        exp = total + parseInt(strArray[i].slice(strArray[i].indexOf("経験値が ") + 4, strArray[i].lastIndexOf(" 上がりました。</font>")), 10);
-                        total = exp;
+    }
+    var reader = new FileReader();
+    // onloadハンドラ
+    reader.onload = function (e) {
+        // 取得した結果を表示
+        var res = e.target.result.replace(/\r\n|\r/g, "\n");
+        var str = res.split("\n");
+        var strArray = new Array();
+        var mobCount = 0;
+        var total = 0;
+        var exp = 0;
+        var flg = true;
+        var firstlog = true;
+        var dateA = "";
+        var dateB = "";
+        var dateC = "";
+        var firstPlayTime = ""; //ログ取得時タイム
+        var lastPlayTime = ""; //最終ログアウト時間
+        var all = 0;
+        var play = 0;
+        var last = 0;
 
-                        if (dateB > dateC) {
-                            all = total;
-                        }
-                    }
+        for (var i = 0; i < str.length; i++) {
+            last = (str.length) - 1;
+            console.log(last)
+            strArray[i] = str[i];
+            //console.log("i:" + i + " strArray:" + strArray[i]);
+
+            //ログ取得開始時間取得
+            if (strArray[i].indexOf("時") > 0 && strArray[i].indexOf("分") > 0) {
+                if (firstlog) {
+                    play = i;
+                    
+                    firstlog = false;
                 }
-                $("#result").html("MOB討伐数は<span>" + String(mobCount).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "</span>匹です");
-                $("#exp").html("総取得経験値は、" + String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "expです<br>狩りをはじめてから１時間の取得経験値は、" +
-                    String(all).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "expです");
-                $("span").attr("id", "count");
-            };
-            reader.readAsText(file, "SJIS");
-        });
+                //プレイ時間ラスト取得
+                if (i == last) {
+                    last = i;
+                    
+                }
 
-        $("#drop-area").on("dragover", function(e) {
-            e.preventDefault();
-        });
+            }
+            if (strArray[i].indexOf("経験値が") > 0) {
+                if (strArray[i].indexOf("ルーン経験値が") > 0) {
+                    continue;
+                }
+                mobCount += 1;
+
+                if (flg == true) {
+                    dateA = new Date(oneDay.getFullYear(), oneDay.getMonth(), oneDay.getDate(), strArray[i].substr(strArray[i].indexOf("[") + 1, 2), strArray[i].substr(strArray[i].indexOf("[") + 5, 2), strArray[i].substr(strArray[i].indexOf("[") + 9, 2));
+                    dateB = new Date(dateA);
+                    dateB.setHours(dateA.getHours() + 1);
+                    flg = false;
+                }
+                dateC = new Date(oneDay.getFullYear(), oneDay.getMonth(), oneDay.getDate(), strArray[i].substr(strArray[i].indexOf("[") + 1, 2), strArray[i].substr(strArray[i].indexOf("[") + 5, 2), strArray[i].substr(strArray[i].indexOf("[") + 9, 2));
+
+                //経験値
+                exp = total + parseInt(strArray[i].slice(strArray[i].indexOf("経験値が ") + 4, strArray[i].lastIndexOf(" 上がりました。</font>")), 10);
+                total = exp;
+
+                if (dateB > dateC) {
+                    all = total;
+                }
+            }
+        }
+        firstPlayTime = new Date(oneDay.getFullYear(), oneDay.getMonth(), oneDay.getDate(), parseInt(strArray[play].substring(strArray[play].indexOf("[") + 1, strArray[play].indexOf("時")), 10), parseInt(strArray[play].substring(strArray[play].indexOf("時") + 1, strArray[play].indexOf("分")), 10), parseInt(strArray[play].substring(strArray[play].indexOf("分") + 1, strArray[play].indexOf("秒")), 10));
+
+        
+
+        lastPlayTime = new Date(oneDay.getFullYear(), oneDay.getMonth(), oneDay.getDate(), parseInt(strArray[last].substring(strArray[last].indexOf("[") + 1, strArray[last].indexOf("時")), 10), parseInt(strArray[last].substring(strArray[last].indexOf("時") + 1, strArray[last].indexOf("分")), 10), parseInt(strArray[last].substring(strArray[last].indexOf("分") + 1, strArray[last].indexOf("秒")), 10));
+        
+
+        var playTimeTotal = lastPlayTime-firstPlayTime;
+        var h = String(Math.floor(playTimeTotal / 3600000) + 100).substring(1);
+        var m = String(Math.floor((playTimeTotal - h * 3600000)/60000)+ 100).substring(1);
+        var s = String(Math.round((playTimeTotal - h * 3600000 - m * 60000)/1000)+ 100).substring(1);
+
+
+        $("#result").html("MOB討伐数は<span>" + String(mobCount).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "</span>匹です");
+        $("#playtime").html("総プレイ時間は、" + h + "時間" + m + "分" + s + "秒です");
+        $("#exp").html("総取得経験値は、" + String(total).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "expです<br>狩りをはじめてから１時間の取得経験値は、" +
+            String(all).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,') + "expです");
+        $("span").attr("id", "count");
+    };
+    reader.readAsText(file, "SJIS");
+});
+
+$("#drop-area").on("dragover", function (e) {
+    e.preventDefault();
+});
